8b192ada39683a8e7f8286832a95197217b571dc
Marcin Malessa <marcin@malessa.pl>
2025-10-09 12:47:09 +0200

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,9 @@ ARG UID=1000
 ARG USER=ubuntu
 RUN useradd -m -u ${UID} ${USER}
 
+ENV PATH="$PATH:/home/${USER}/.dotnet/tools"
+
 USER ${USER}
+RUN dotnet tool install --global dotnet-ef
+
 WORKDIR /app
--- src/LearningCSharp.Domain/Events/OrderCreated.cs
+++
@@ -1,6 +0,0 @@
-namespace LearningCSharp.Domain.Events;
-
-public record OrderCreated(Guid OrderId, string CustomerId, decimal Total)
-{
-    public DateTime OccurredAt { get; init; } = DateTime.UtcNow;
-}
